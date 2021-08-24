23838ee660f9169a6b741ea73ff5b0513e1c41b7
Anzel Capparelli <anzelcapparelli@gmail.com>
2021-08-23 18:35:20 -0700

    initial java programs (hello world, variable types)

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int a=1;
+        double b=1.2;
+        double sum=a+b;
+
+        String c="num";
+        String catnum=c+a;
+        System.out.println(sum);
+        System.out.println(catnum);
+    }
+}
